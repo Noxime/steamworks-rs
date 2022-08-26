36de7f1da54ff7ccb65be04d5b3c2f6d3a70558f
Kitlith <kitlith@kitl.pw>
2022-08-26 14:56:45 -0700

    GetConnectedControllers assumes at least max count elements
    Steamworks assumes that there are STEAM_INPUT_MAX_COUNT elements
    available, and could write out of bounds if we do not have enough
    elements allocated.

--- src/input.rs
+++ src/input.rs
@@ -43,7 +43,7 @@ impl<Manager> Input<Manager> {
     /// Returns a list of the currently connected controllers without allocating
     pub fn get_connected_controllers_slice(&self, mut controllers: impl AsMut<[InputHandle_t]>) {
         let handles = controllers.as_mut();
-        assert!(handles.len() <= sys::STEAM_INPUT_MAX_COUNT as usize);
+        assert!(handles.len() >= sys::STEAM_INPUT_MAX_COUNT as usize);
         unsafe {
             sys::SteamAPI_ISteamInput_GetConnectedControllers(self.input, handles.as_mut_ptr());
         }
